d18cdd7ab80761ee208800a453408e516fc2f583
jonbos-palmer <jbos@thepalmergroup.com>
2020-06-23 16:15:22 -0500

    add switch for error handling

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ import { Error } from "./pages/Error";
 function App() {
   return (
     <>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
